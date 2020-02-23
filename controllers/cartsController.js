6055f09ca985c4c1383160e4e8ac7ab99b9f5835
VovaSidorov <VovaCivic@gmail.com>
2020-02-23 21:33:52 +0200

    reate add to cart

--- controllers/cartsController.js
+++ controllers/cartsController.js
@@ -1,6 +1,9 @@
 const CartItem = require('../models').CartItem;
 const Product = require('../models').Product;
 
+const CartItem = require('../models').CartItem;
+const Product = require('../models').Product;
+
 exports.cart_list = (req, res) => {
 
     const {params: {userId}} = req;
@@ -17,4 +20,36 @@ exports.cart_list = (req, res) => {
                 }
             });
         });
+};
+
+exports.add_to_cart = (req, res) => {
+
+    const {params: {userId,productId}} = req;
+
+    console.log(userId,productId);
+
+    CartItem.findOne({ where: {userId,productId} })
+        .then(function(obj) {
+            if(obj) { // update
+                console.log("update",JSON.stringify(obj));
+                obj.increment('amount');
+                return res.json({
+                    status: "ok",
+                    data:{
+                        cartItems: obj,
+                    }
+                });
+            }
+            else { // insert
+                console.log("insert",JSON.stringify(obj));
+                const newItem = CartItem.create({userId,productId,amount:1});
+                return res.json({
+                    status: "ok",
+                    data:{
+                        cartItems: newItem,
+                    }
+                });
+            }
+        });
+
 };
